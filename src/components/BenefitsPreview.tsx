c71bd46c26ce1a9ef887597fd7584779d4f70e82
jetpackgone <jetpackgone.dev@gmail.com>
2021-03-15 23:08:20 -0400

    Updated BenefitsPreview to use table

--- src/components/BenefitsPreview.tsx
+++ src/components/BenefitsPreview.tsx
@@ -2,14 +2,28 @@ import React from "react";
 import * as benefits from '../core/benefits';
 
 export class BenefitsPreview extends React.Component <{ employeeName: string, dependentNames: string[] }, {}> {
-  constructor(props: any) {
-    super(props);
-  }
-
   render() {
+    const annualCost = benefits.calculateAnnualCost(this.props.employeeName, this.props.dependentNames);
     return (
       <div>
-        Benefits Deduction: ${benefits.calculateAnnualCost(this.props.employeeName, this.props.dependentNames)}
+        <h2>Benefits Deduction Preview</h2>
+        <table>
+          <tr>
+            <th></th>
+            <th>Annually</th>
+            <th>Per Paycheck</th>
+          </tr>
+          <tr>
+            <td>Deduction</td>
+            <td>${annualCost}</td>
+            <td>${benefits.convertToPerPaycheckCost(annualCost)}</td>
+          </tr>
+          <tr>
+            <td>Gross Salary</td>
+            <td>${benefits.calculateGrossSalary(annualCost)}</td>
+            <td>${benefits.calculateGrossPaycheck(annualCost)}</td>
+          </tr>
+        </table>
       </div>
     );
   }
